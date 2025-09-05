0864b18128eaec86947176d5eed6fd32d8ee9b97
ryanhess <hess29@gmail.com>
2025-09-05 12:48:42 -0400

    revision 1

--- file.py
+++ file.py
@@ -6,6 +6,16 @@ hostName = "localhost"
 serverPort = 8080
 
 class MyServer(BaseHTTPRequestHandler):
+    def do_HEAD(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
+        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
+        self.wfile.write(bytes("<body>", "utf-8"))
+        self.wfile.write(bytes("<p>Hello universe.</p>", "utf-8"))
+        self.wfile.write(bytes("</body></html>", "utf-8"))
+        
     def do_GET(self):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
